6f2a7bf011d60d0fd0988bcbb8e9e863e209b2a0
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-04-04 13:44:39 +0000

    list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,56 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import "../../styles/index.css";
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState("");
+	const [todoList, setTodolist] = useState([]);
+
+	const line = (X) => {
+		const newList = todoList.filter((element, i) => X !== i);
+		setTodolist(newList);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<input
+					type="text"
+					className="fore-control"
+					placeholder="username"
+					onChange={(e) => setItem(e.target.value)}
+					value={item}
+				/>
+				<a
+					onClick={() => {
+						if (item !== "") {
+							setTodolist([...todoList, item]);
+							setItem("");
+						}
+					}}
+					type="button"
+					className="btn btn-primary"
+					id="basic-addon1">
+					Button
+				</a>
+			</div>
+			<ul>
+				{todoList.map((element, index) => {
+					return (
+						<>
+							<li key={index} className="mr-2">
+								{element}
+								<a
+									className="m1-2 btn btn-danger"
+									onClick={() => {
+										line(index);
+									}}>
+									X
+								</a>
+							</li>
+						</>
+					);
+				})}
+			</ul>
+		</>
 	);
 };
 
